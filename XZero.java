6d2ce8d78db8edd87b36c6575a1f6959ac5e07b3
asdadsdasd <mkozarez12345@mail.ru>
2023-02-26 17:57:25 +0300

    initial commit

---
+++ XZero.java
@@ -0,0 +1,13 @@
+package xzero;
+
+import javax.swing.*;
+
+import xzero.view.GamePanel;
+
+public class XZero {
+
+    public static void main(String[] args) {
+        JFrame app = new GamePanel();
+        app.setVisible(true);
+    }
+}
